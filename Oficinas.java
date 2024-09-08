8bc8bd90632951478d9328dc823c89af1b335a46
Rodrigo Freitas <164232597+rodrigosfreita@users.noreply.github.com>
2024-09-08 17:30:12 -0300

    Oficinas.java

--- Oficinas.java
+++ Oficinas.java
@@ -1,120 +1,113 @@
 package model;
 
-public class Oficinas {
-	
+public class Workshops {
 
-		private Orçamento orcamento;
-		private int numeroOficinas;
-		private String endereco;
-		private long telefone;
-		private int cepOficinas;
-		private long cnpjOficina;
-		private String nomeFantasia;
-		private int inscricaoEstadual;
-		private String forencedorPecas;
+    private Budget budget;
+    private int workshopNumber;
+    private String address;
+    private long phone;
+    private int postalCode;
+    private long cnpj;
+    private String tradeName;
+    private int stateRegistration;
+    private String partsSupplier;
 
-		
-		public Oficinas(Orçamento orcamento, int numeroOficinas, String endereco,
-				long telefone, int cepOficinas, long cnpjOficina, String nomeFantasia,
-				int inscricaoEstadual, String forencedorPecas) {		
-			this.orcamento = orcamento;
-			this.numeroOficinas = numeroOficinas;
-			this.endereco = endereco;
-			this.telefone = telefone;
-			this.cepOficinas = cepOficinas;
-			this.cnpjOficina = cnpjOficina;
-			this.nomeFantasia = nomeFantasia;
-			this.inscricaoEstadual = inscricaoEstadual;
-			this.forencedorPecas = forencedorPecas;
-			
-			System.out.println("Construtor cheio - objeto Oficinas criado!");
+    public Workshops(Budget budget, int workshopNumber, String address, long phone, int postalCode,
+                     long cnpj, String tradeName, int stateRegistration, String partsSupplier) {
+        this.budget = budget;
+        this.workshopNumber = workshopNumber;
+        this.address = address;
+        this.phone = phone;
+        this.postalCode = postalCode;
+        this.cnpj = cnpj;
+        this.tradeName = tradeName;
+        this.stateRegistration = stateRegistration;
+        this.partsSupplier = partsSupplier;
 
-		}
-		
-		public Oficinas() {
-			System.out.println("Construtor vazio - objeto Oficinas criado!");
-		};
-		
-		public Orçamento getOrcamento() {
-			return orcamento;
-		}
-		
-		public void setOrçamento (Orçamento orcamento) {
-			this.orcamento = orcamento;
-		}
-		
-		public int getNumeroOficinas() {
-			return numeroOficinas;
-		}
-		
-		public void setNumeroOficinas(int numeroOficinas) {	
-			this.numeroOficinas = numeroOficinas;
-		}
-		
-		public String getEndereco() {
-			return endereco;
-		}
-		
-		public void setEndereco(String endereco) {
-			this.endereco = endereco;
-		}
-		
-		public long getTelefone() {
-			return telefone;
-		}
-		
-		public void setTelefone(long telefone) {
-			this.telefone = telefone;
-		}
-		
-		public int getCepOficinas() {
-			return cepOficinas;
-		}
-		
-		public void setCepOficinas(int cepOficinas) {
-			this.cepOficinas = cepOficinas;
-		}
-		
-		public  long getCnpjOficina() {
-			return cnpjOficina;
-		}
-		
-		public void setCnpjOficina(long cnpjOficina) {
-			this.cnpjOficina = cnpjOficina;
-		}
-		
-		public String getNomeFantasia() {
-			return nomeFantasia;
-		}
-		
-		public void setNomeFantasia(String nomeFantasia) {
-			this.nomeFantasia = nomeFantasia;
-		}
-		
-		public int getInscricaoEstadual() {
-			return inscricaoEstadual;
-		}
-		
-		public void setInscricaoEstadual(int inscricaoEstadual) {
-			this.inscricaoEstadual = inscricaoEstadual;
-		}
-		
-		public String getForencedorPecas() {
-			return forencedorPecas;
-		}
-		
-		public void setForencedorPecas (String forencedorPecas) {
-			this.forencedorPecas = forencedorPecas;
-		}
-		
-		@Override
-		public String toString (){
-			return "Oficinas[orcamento=" + orcamento + ",numeroOficinas=" + numeroOficinas + 
-					",endereco="+ endereco +",telefone=" + telefone + ",cepOficinas=" + cepOficinas + ","
-				+ "cnpjOficina=" + cnpjOficina + ",nomeFantasia=" + nomeFantasia +",inscricaoEstadual=" + inscricaoEstadual + ","
-						+ "forencedorPecas="+ forencedorPecas +"]";
-		}
-		
+        System.out.println("Full constructor - Workshops object created!");
+    }
 
+    public Workshops() {
+        System.out.println("Empty constructor - Workshops object created!");
+    }
 
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Budget budget) {
+        this.budget = budget;
+    }
+
+    public int getWorkshopNumber() {
+        return workshopNumber;
+    }
+
+    public void setWorkshopNumber(int workshopNumber) {
+        this.workshopNumber = workshopNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTradeName() {
+        return tradeName;
+    }
+
+    public void setTradeName(String tradeName) {
+        this.tradeName = tradeName;
+    }
+
+    public int getStateRegistration() {
+        return stateRegistration;
+    }
+
+    public void setStateRegistration(int stateRegistration) {
+        this.stateRegistration = stateRegistration;
+    }
+
+    public String getPartsSupplier() {
+        return partsSupplier;
+    }
+
+    public void setPartsSupplier(String partsSupplier) {
+        this.partsSupplier = partsSupplier;
+    }
+
+    @Override
+    public String toString() {
+        return "Workshops[budget=" + budget + ", workshopNumber=" + workshopNumber + ", address=" + address +
+               ", phone=" + phone + ", postalCode=" + postalCode + ", cnpj=" + cnpj +
+               ", tradeName=" + tradeName + ", stateRegistration=" + stateRegistration +
+               ", partsSupplier=" + partsSupplier + "]";
+    }
 }
