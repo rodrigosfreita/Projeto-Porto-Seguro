34abbd29c8a26f9b61c420387a18aeb7b18ed20d
Rodrigo Freitas <164232597+rodrigosfreita@users.noreply.github.com>
2024-09-08 17:40:28 -0300

    analiseDeFalhas.java

--- analiseDeFalhas.java
+++ analiseDeFalhas.java
@@ -1,13 +1,13 @@
 package model;
 
-public class analiseDeFalhas {
-	
-	public AutoAvaliação avaliarVeiculo(int opcao) {
-		
-		while(opcao!=0) {
-			
-		}
-		return null;
-	}
+public class FailureAnalysis {
+
+    public SelfEvaluation evaluateVehicle(int option) {
+
+        while(option != 0) {
+            // Logic for evaluating the vehicle can be added here
+        }
+        return null;
+    }
 
 }
