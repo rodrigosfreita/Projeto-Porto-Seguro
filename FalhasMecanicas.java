aceff467585ac145b6a423e71402ef324a999c77
Rodrigo Freitas <164232597+rodrigosfreita@users.noreply.github.com>
2024-09-08 17:25:23 -0300

    FalhasMecanicas.java

--- FalhasMecanicas.java
+++ FalhasMecanicas.java
@@ -1,121 +1,114 @@
 package model;
 
-public class FalhasMecanicas {
-	
+public class MechanicalFailures {
+    
+    private Vehicle vehicle;
+    private boolean overheating;
+    private boolean ignition;
+    private boolean noisesAndRattles;
+    private boolean increasedFuelConsumption;
+    private boolean blackSmoke;
+    private boolean powerLoss;
+    private boolean waterTank;
+    private boolean increasedTemperature;
 
-		private Veiculo veiculos;
-		private boolean superAquecimento;
-		private boolean ingnicao;
-		private boolean barulhosRuidos ;
-		private boolean aumentoCombustivel;
-		private boolean fumacaPreta;
-		private boolean perdaPotencia;
-		private boolean reservatorioAgua;
-		private boolean aumentoTemperatura;
-		
-		
-		public FalhasMecanicas(Veiculo veiculos, boolean superAquecimento, boolean ingnicao, boolean barulhosRuidos,
-				boolean aumentoCombustivel, boolean fumacaPreta, boolean perdaPotencia, boolean reservatorioAgua,
-				boolean aumentoTemperatura) {	
-			
-			this.veiculos = veiculos;
-			this.superAquecimento = superAquecimento;
-			this.ingnicao = ingnicao;
-			this.barulhosRuidos = barulhosRuidos;
-			this.aumentoCombustivel = aumentoCombustivel;
-			this.fumacaPreta = fumacaPreta;
-			this.perdaPotencia = perdaPotencia;
-			this.reservatorioAgua = reservatorioAgua;
-			this.aumentoTemperatura = aumentoTemperatura;
-			
-			System.out.println("Construtor cheio - objeto Veiculo criado!");
+    public MechanicalFailures(Vehicle vehicle, boolean overheating, boolean ignition, boolean noisesAndRattles,
+                              boolean increasedFuelConsumption, boolean blackSmoke, boolean powerLoss, 
+                              boolean waterTank, boolean increasedTemperature) {
+        this.vehicle = vehicle;
+        this.overheating = overheating;
+        this.ignition = ignition;
+        this.noisesAndRattles = noisesAndRattles;
+        this.increasedFuelConsumption = increasedFuelConsumption;
+        this.blackSmoke = blackSmoke;
+        this.powerLoss = powerLoss;
+        this.waterTank = waterTank;
+        this.increasedTemperature = increasedTemperature;
 
-		}
-		
-		public FalhasMecanicas() {
-			System.out.println("Construtor vazio - objeto Veiculo criado!");
-		};
-		
-		public Veiculo getVeiculos() {
-			return veiculos;
-		}
-		
-		public void setVeiculos (Veiculo veiculos) {
-			this.veiculos = veiculos;
-		}
-		
-		public boolean getSuperAquecimento() {
-			return superAquecimento;
-		}
-		
-		public void setSuperAquecimento(boolean superAquecimento) {	
-			this.superAquecimento = superAquecimento;
-		}
-		
-		public boolean getIngnicao() {
-			return ingnicao;
-		}
-		
-		public void setIngnicao(boolean ingnicao) {
-			this.ingnicao = ingnicao;
-		}
-		
-		public boolean getBarulhosRuidos() {
-			return barulhosRuidos;
-		}
-		
-		public void setBarulhosRuidos(boolean barulhosRuidos) {
-			this.barulhosRuidos = barulhosRuidos;
-		}
-		
-		public boolean getAumentoCombustivel() {
-			return aumentoCombustivel;
-		}
-		
-		public void setAumentoCombustivel(boolean aumentoCombustivel) {
-			this.aumentoCombustivel = aumentoCombustivel;
-		}
-		
-		public  boolean getFumacaPreta() {
-			return fumacaPreta;
-		}
-		
-		public void setFumacaPreta(boolean fumacaPreta) {
-			this.fumacaPreta = fumacaPreta;
-		}
-		
-		public boolean getPerdaPotencia() {
-			return perdaPotencia;
-		}
-		
-		public void setPerdaPotencia(boolean perdaPotencia) {
-			this.perdaPotencia = perdaPotencia;
-		}
-		
-		public boolean getReservatorioAgua() {
-			return reservatorioAgua;
-		}
-		
-		public void setReservatorioAgua(boolean reservatorioAgua) {
-			this.reservatorioAgua = reservatorioAgua;
-		}
-		
-		public boolean getAumentoTemperatura() {
-			return aumentoTemperatura;
-		}
-		
-		public void setAumentoTemperatura (boolean aumentoTemperatura) {
-			this.aumentoTemperatura = aumentoTemperatura;
-		}
-		
-		@Override
-		public String toString (){
-			return "FalhasMecanicas[veiculos=" + veiculos + ",superAquecimento=" + superAquecimento + ",ingnicao="+ 
-					ingnicao +",barulhosRuidos=" + barulhosRuidos + ",aumentoCombustivel=" + aumentoCombustivel + ",fumacaPreta="
-				+ "" + fumacaPreta + ",perdaPotencia=" + perdaPotencia +",reservatorioAgua=" + reservatorioAgua + 
-				",aumentoTemperatura="+ aumentoTemperatura +"]";
-		}
-		
+        System.out.println("Full constructor - MechanicalFailures object created!");
+    }
 
+    public MechanicalFailures() {
+        System.out.println("Empty constructor - MechanicalFailures object created!");
+    }
 
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public boolean getOverheating() {
+        return overheating;
+    }
+
+    public void setOverheating(boolean overheating) {
+        this.overheating = overheating;
+    }
+
+    public boolean getIgnition() {
+        return ignition;
+    }
+
+    public void setIgnition(boolean ignition) {
+        this.ignition = ignition;
+    }
+
+    public boolean getNoisesAndRattles() {
+        return noisesAndRattles;
+    }
+
+    public void setNoisesAndRattles(boolean noisesAndRattles) {
+        this.noisesAndRattles = noisesAndRattles;
+    }
+
+    public boolean getIncreasedFuelConsumption() {
+        return increasedFuelConsumption;
+    }
+
+    public void setIncreasedFuelConsumption(boolean increasedFuelConsumption) {
+        this.increasedFuelConsumption = increasedFuelConsumption;
+    }
+
+    public boolean getBlackSmoke() {
+        return blackSmoke;
+    }
+
+    public void setBlackSmoke(boolean blackSmoke) {
+        this.blackSmoke = blackSmoke;
+    }
+
+    public boolean getPowerLoss() {
+        return powerLoss;
+    }
+
+    public void setPowerLoss(boolean powerLoss) {
+        this.powerLoss = powerLoss;
+    }
+
+    public boolean getWaterTank() {
+        return waterTank;
+    }
+
+    public void setWaterTank(boolean waterTank) {
+        this.waterTank = waterTank;
+    }
+
+    public boolean getIncreasedTemperature() {
+        return increasedTemperature;
+    }
+
+    public void setIncreasedTemperature(boolean increasedTemperature) {
+        this.increasedTemperature = increasedTemperature;
+    }
+
+    @Override
+    public String toString() {
+        return "MechanicalFailures[vehicle=" + vehicle + ", overheating=" + overheating + ", ignition=" + ignition +
+               ", noisesAndRattles=" + noisesAndRattles + ", increasedFuelConsumption=" + increasedFuelConsumption + 
+               ", blackSmoke=" + blackSmoke + ", powerLoss=" + powerLoss + ", waterTank=" + waterTank + 
+               ", increasedTemperature=" + increasedTemperature + "]";
+    }
 }
